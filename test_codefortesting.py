16524851a1523a27dfdf61fce5e164001c614d38
Kristian Wink <kristian.wink@gmail.com>
2023-11-29 11:34:03 +0200

    Added pytest file

---
+++ test_codefortesting.py
@@ -0,0 +1,11 @@
+import pytest
+import codefortesting
+
+def test_answer():
+    assert codefortesting.add(3, 5) == 8
+
+def test_answer():
+    assert codefortesting.multiply(2,5) == 10
+
+def test_answer():
+    assert codefortesting.power(2,8) == 256
